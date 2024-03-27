773f4e31884f217e398189c65f131d16306d7453
sachinSingh53 <20it095@jssaten.ac.in>
2024-03-27 19:16:20 +0530

    added verify-email functionality

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import { create } from '../controllers/signup.js';
 import { verifyGatewayRequest } from '../../../9-jobber-shared/src/gateway-middleware.js';
-
-const router = express.Router();
-
 import { authChannel } from '../app.js';
 import { read } from '../controllers/signin.js';
-
+import { update } from '../controllers/verifyEmail.js';
+const router = express.Router();
 // router.get('/check')
 
 router.post('/signup', verifyGatewayRequest, create);   
 router.post('/signin', verifyGatewayRequest, read);   
+router.put('/verify-email', verifyGatewayRequest, update);   
 // router.post('/signup', create);   
 
 export default router;
